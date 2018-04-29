cbfef0615fc3af418538b133e68d1dd8d315fadd
felipeballesteros <felipeballesteros@msn.com>
2018-04-29 08:43:25 -0400

    Added check for existing record Id on the db

--- services/passport.js
+++ services/passport.js
@@ -14,9 +14,16 @@ passport.use(
 			callbackURL: "/auth/google/callback"
 		},
 		(accessToken, refreshToken, profile, done) => {
-			new User({
-				googleId: profile.id
-			}).save(); // save to the db
+			// Try to find the user record first,
+			// Returns a promise
+			User.findOne({ googleId: profile.id }).then(existingUser => {
+				if (existingUser) {
+					// We already have a record with the given profile Id
+				} else {
+					// We don't have a user record with the id, create one
+					new User({ googleId: profile.id }).save(); // save to the db
+				}
+			});
 		}
 	)
 );
