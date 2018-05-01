1487f289a5a10658d9d25eceaaa48494247992f3
felipeballesteros <felipe.ballesteros@cbc.ca>
2018-05-01 06:28:58 -0400

    Creating route for testing google Oauth authentication

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,4 +12,8 @@ module.exports = app => {
 
 	// Create a route for handling google callback
 	app.get("/auth/google/callback", passport.authenticate("google"));
+
+	app.get("/api/current_user", (req, res) => {
+		res.send(req.user);
+	});
 };
